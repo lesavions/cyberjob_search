5a004c2a848c51a53a78ca4f8de2f7bc983ade75
Martine Brangan <111652824+lesavions@users.noreply.github.com>
2022-08-22 14:09:08 +0100

    Update cyberjob_search.py

--- cyberjob_search.py
+++ cyberjob_search.py
@@ -5,10 +5,10 @@ Created on Sun Aug 21 22:17:36 2022
 @author: marti
 """
 
-#SEARCH NEWLY POSTED REMOTE ONLY MID_SENIOR LEVEL CYBERSECUITY ROLES IN IRELAND
+#SEARCH RECENTLY POSTED, REMOTE ONLY, MID-SENIOR LEVEL PENETRATION TESTER ROLES IN IRELAND
 
-#Below enter path for saving screenshot
-save_path = 'C:/Users/marti/cyberjob_new.png'
+#Below enter path for saving screenshot if required
+save_path = 'PATH/cyberjob_new.png'
 
 from selenium import webdriver
 import requests
@@ -17,7 +17,7 @@ import time
 
 driver = webdriver.Chrome('C:/Users/marti/chromedriver.exe')
 
-url = 'https://ie.linkedin.com/jobs/cybersecurity-jobs?keywords=Cybersecurity&location=Ireland&locationId=&geoId=104738515&f_TPR=r86400&f_E=3%2C4%2C5&f_WT=2&position=1&pageNum=00'
+url = 'https://ie.linkedin.com/jobs/search?keywords=Penetration%20Tester&location=Ireland&locationId=&geoId=104738515&f_TPR=r604800&f_E=3%2C4&f_WT=2&position=1&pageNum=0'
 
 driver.get(url)
 time.sleep(1)
@@ -31,18 +31,16 @@ page = requests.get(url)
 soup = BeautifulSoup(page.text, 'lxml')
 soup
 
-#finds newly posted jobs
+#finds recently posted jobs
 postings = soup.find_all('div', class_= 'base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card job-search-card--active')
 jobs = []
 while True:
     for post in postings:
         jobs.append(post.text)
-    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-    time.sleep(2)
     soup = BeautifulSoup(driver.page_source, 'lxml')
     postings = soup.find_all('div', class_= 'base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card job-search-card--active')
     jobs = list(set(jobs))
-    if len(jobs) > 10:
+    if len(jobs) > 20:
         break
     
 driver.save_screenshot(save_path)
